2477b463e3b47c2854447f21848aa66d03c614ea
eka <21106050023@student.uin-suka.ac.id>
2024-01-12 20:13:08 +0700

    fix: footer done

--- src/scripts/component/footer.js
+++ src/scripts/component/footer.js
@@ -11,14 +11,18 @@ class FooterCo extends HTMLElement {
             <div class="flex relative h-16">
                 <div class="absolute inset-0 bg-black bg-opacity-50"></div>
                 <img src="${mainimg}" class="flex w-full h-full object-cover"></img>
-                <div class="text-white font-bold absolute inset-0 flex flex-col m-auto justify-center w-full items-center>
-                    <img src="${logo}" class="flex h-12"></img>
-                    <div class="flex justify-center space-x-10">
-                        <a href="#home" class="hover:text-light_purple">Home</a>
-                        <a href="#movies" class="hover:text-light_purple">Movies</a>
-                        <a href="#search" class="hover:text-light_purple">Search</a>
-                        <a href="#about-us" class="hover:text-light_purple">About Us</a>      
-                    </div>
+                <div class="text-white font-bold absolute inset-0 flex flex-col m-auto justify-center w-full h-fit items-center>
+                    <div class="flex justify-center flex-col">
+                        <div class="flex logo-container m-auto h-12 w-auto">
+                            <img src="${logo}" class="h-full object-contain"></img>
+                        </div>                        
+                        <div class="flex justify-center space-x-10 ">
+                            <a href="#home" class="hover:text-light_purple">Home</a>
+                            <a href="#movies" class="hover:text-light_purple">Movies</a>
+                            <a href="#search" class="hover:text-light_purple">Search</a>
+                            <a href="#about-us" class="hover:text-light_purple">About Us</a>      
+                        </div>
+                    </div>    
                 </div>
             </div>
             <div class="flex justify-center bg-dark_blue text-white  align-middle">
